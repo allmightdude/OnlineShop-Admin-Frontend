c440802c6b67d405e29c6ef8b49a6b88fe8683f4
allmightdude <sabzi_f@yahoo.com>
2022-12-24 14:48:31 +0330

    Button Global slot

---
+++ components/BaseButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <button class="btn" :class="[mode , border , size]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      required: false,
+      default : 'orange'
+    },
+    border: {
+      type: String,
+      required: false,
+    },
+    size: {
+      type: String,
+      required: false,
+      default : 'lg-size'
+    },
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  border-radius: 5px;
+  color: #2c3e50;
+  border: none;
+  background-color: transparent;
+}
+
+.orange {
+  background-color: #f1c40f;
+  border: 1px solid #c2c2c2;
+}
+
+.btn__grey {
+  background-color: #f2f2f2;
+}
+
+.sm-size {
+  padding: 0.5rem;
+}
+
+.border{
+    border: 1px solid #C2C2C2;
+}
+.lg-size {
+  padding: .5rem 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
 <template>
-  <Tutorial/>
+  <main>
+    <h1>All products</h1>
+    <div class="actions mt-2">
+      <base-button mode="orange" size="lg-size">Add new product</base-button>
+      <base-button mode="border" size="lg-size">Add new category</base-button>
+      <base-button mode="border" size="lg-size">Add new owner</base-button>
+    </div>
+  </main>
 </template>
 
-<script>
-export default {}
-</script>
+<style lang="scss">
+  * ,
+  ::before,
+  ::after{
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;;
+  }
+
+  body{
+    box-sizing: border-box;
+    font-size: 1.5rem;
+    padding: 2rem;
+  }
+
+  html{
+    font-size: 62.5%;
+  }
+
+  .mt-1{margin-top: 1rem;}
+  .mt-2{margin-top: 2rem;}
+  .mt-3{margin-top: 3rem;}
+
+  h1{
+    color: #707070;
+  }
+</style>
